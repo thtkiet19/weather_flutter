379d117020d63e274f1a01e77456caeb490f85fe
thtkiet19 <63939319+thtkiet19@users.noreply.github.com>
2021-07-23 16:56:34 +0700

    grey screen on dailay forecast fixed

--- lib/render/load_daily_weather.dart
+++ lib/render/load_daily_weather.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
-import 'animatedImage.dart';
 import '../models/daily_models.dart';
 import 'package:bordered_text/bordered_text.dart';
 
@@ -41,89 +40,86 @@ class _LoadDailyWeatherState extends State<LoadDailyWeather> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10),
-      child: Expanded(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              for (int i = 0; i < widget.response!.daily.length; ++i)
-                Row(
-                  children: [
-                    Container(
-                      height: 200,
-                      width: 180,
-                      decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Colors.white70, width: 2)),
-                      //color: Colors.green,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Column(
-                            children: [
-                              Image.asset(
-                                'assets/images/${widget.response!.daily[i].weather[0].icon}.png',
-                                width: 50,
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  BorderedText(
-                                    strokeWidth: 3.0,
-                                    child: Text(
-                                      '${DateFormat.MMMd().format(now.add(Duration(seconds: widget.response!.daily[i].dt)))}',
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          letterSpacing: 4,
-                                          fontWeight: FontWeight.w900,
-                                          color: Colors.white),
-                                    ),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            for (int i = 0; i < widget.response!.daily.length; ++i)
+              Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 180,
+                    decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.white70, width: 2)),
+                    //color: Colors.green,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Image.asset(
+                              'assets/images/${widget.response!.daily[i].weather[0].icon}.png',
+                              width: 50,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                BorderedText(
+                                  strokeWidth: 3.0,
+                                  child: Text(
+                                    '${DateFormat.MMMd().format(now.add(Duration(seconds: widget.response!.daily[i].dt)))}',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        letterSpacing: 4,
+                                        fontWeight: FontWeight.w900,
+                                        color: Colors.white),
                                   ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  BorderedText(
-                                    strokeWidth: 2.0,
-                                    strokeColor: Colors.black87,
-                                    child: Text(
-                                      '${widget.response!.daily[i].temp.day}°C',
-                                      style: GoogleFonts.openSans(
-                                          color: Colors.white,
-                                          fontSize: 30,
-                                          fontWeight: FontWeight.w700),
-                                    ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                BorderedText(
+                                  strokeWidth: 2.0,
+                                  strokeColor: Colors.black87,
+                                  child: Text(
+                                    '${widget.response!.daily[i].temp.day}°C',
+                                    style: GoogleFonts.openSans(
+                                        color: Colors.white,
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.w700),
                                   ),
-                                ],
-                              ),
-                              Text(
-                                widget
-                                    .response!.daily[i].weather[0].description,
-                                style: GoogleFonts.openSans(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              widget.response!.daily[i].weather[0].description,
+                              style: GoogleFonts.openSans(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      width: 10,
-                    )
-                  ],
-                ),
-            ],
-          ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  )
+                ],
+              ),
+          ],
         ),
       ),
     );
